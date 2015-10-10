7b57423cbc0c11f94683bac9c5ea4cb17292687a
yucheng <yucheng@ltzero.local>
2015-10-10 15:44:23 +0800

    Release 1.0.0

---
+++ Push/Push.h
@@ -0,0 +1,13 @@
+//
+//  Push.h
+//  UtilsKitDemo
+//
+//  Created by yucheng on 15/10/10.
+//  Copyright © 2015年 yucheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Push : NSObject
+
+@end
